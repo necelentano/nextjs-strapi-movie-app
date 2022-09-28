b04bfe5b7285f5fdd87a704fc0a23379ad64433e
necelentano <necelentano@gmail.com>
2022-09-28 22:39:49 +0300

    TailwindCss Setup. Added Layout & Nav components

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Nav from "./Nav";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Movie Database</title>
+      </Head>
+      <Nav />
+      <main>
+        <div className="px-4">
+          <div className="flex justify-center items-center bg-white mx-auto w-2/4 rounded-lg my-16 p-16">
+            <div className="text-2xl font-medium">{children}</div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
