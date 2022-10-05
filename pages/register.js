324d864e752e518d7098318cb0e80141672f73bc
necelentano <necelentano@gmail.com>
2022-10-05 22:39:07 +0300

    Added New User Registration Form

---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+import RegisterForm from "../components/RegisterForm";
+
+const Register = () => {
+  return (
+    <Layout>
+      <RegisterForm />
+    </Layout>
+  );
+};
+
+export default Register;
